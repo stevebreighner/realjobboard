d56983b7fb300327705a33476f9a56141697e2c7
Steve Breighner <steve.breighner@gmail.com>
2025-10-28 16:20:36 -0400

    Restore checkout.php with environment variable keys

--- wp-content/themes/jobs/checkout.php
+++ wp-content/themes/jobs/checkout.php
@@ -1,8 +1,7 @@
-<?php
+tr<?php
     require_once get_template_directory() . '/stripe/init.php';
-    \Stripe\Stripe::setApiKey('sk_test_51P3zmJHlusB1EIZ8UedPKPM7dgISnZ5xiUNIulWdWp4djeJ7pLlp7GGXe5c8JIxM4EkPZGoMeaArda5D6hIJRhxC00DgCr6cEG');
+    \Stripe\Stripe::setApiKey(STRIPE_SECRET_KEY);
 
-    // \Stripe\Stripe::setApiKey('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');
 
 
 if (!isset($_POST['price_id'])) {
